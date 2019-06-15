f703716c54e97ce7ac80452f5da63d65a685778b
Eduardo <barajas.eduardo@uabc.edu.mx>
2019-06-15 08:00:24 -0700

    Se modifico la entidad de usuario.

--- BackendAPI/src/main/java/com/CODEns/BackendAPI/Entities/User.java
+++ BackendAPI/src/main/java/com/CODEns/BackendAPI/Entities/User.java
@@ -1,6 +1,16 @@
 package com.CODEns.BackendAPI.Entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id_user;
     private String name;
     private String username;
@@ -10,7 +20,10 @@ public class User {
     private int role;
 
     public User(String user) {
-        this.username = user;
+        this.name = user;
+    }
+    public User() {
+
     }
 
     public String getName() { return name; }
@@ -27,4 +40,17 @@ public class User {
 
     public int getRole() { return role; }
 
+    public void setName(String name) { this.name = name; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public void setId(int id_user) { this.id_user = id_user; }
+
+    public void setPrivateKey(String private_key) { this.private_key = private_key; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public void setCreationDate(String date) { this.creation_date = date; }
+
+    public void setRole(int role) { this.role = role; }
 }
