b6e5deec47ba85afbe6223dd4dd54a66d70bb2e9
Eduardo <barajas.eduardo@uabc.edu.mx>
2019-07-02 21:10:53 -0700

    Se elimino el modulo para las sugestiones.

--- BackendAPI/src/main/java/com/CODEns/BackendAPI/Repositories/MovieSuggestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.CODEns.BackendAPI.Repositories;
-import org.springframework.data.repository.CrudRepository;
-
-import com.CODEns.BackendAPI.Entities.MovieSuggestion;
-
-public interface MovieSuggestionRepository extends CrudRepository<MovieSuggestion, Integer> {
-
-}
